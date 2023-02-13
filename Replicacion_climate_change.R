fae19f95cce93883f5312a961ca5bda69dec3377
climate2023 <climate_financial_br@outlook.com>
2023-02-13 09:29:06 -0500

    Update Replicacion_climate_change.R

--- Replicacion_climate_change.R
+++ Replicacion_climate_change.R
@@ -1,86 +1,73 @@
-
 rm(list = ls())
 
 ## Libraries
-install.packages("base")
+install.packages("tsbox")
 library(lubridate)
 library(xts)
 library(tidyr)
 library(timeDate)
-
 library(zoo)
+library(tempdisagg)
+library(readxl)
+library(tsbox)
 
-## Read CSV files
-#aus_csv <- read.csv("C:/Users/jpber/OneDrive/Documents/BanRep/Replicacion/Bases/Indices_completos/Stocks_Australia.csv")
-bel_csv <- read.csv("C:/Users/jpber/OneDrive/Documents/BanRep/Replicacion/Bases/Indices_completos/Stocks_Belgium.csv")
-bra_csv <- read.csv("C:/Users/jpber/OneDrive/Documents/BanRep/Replicacion/Bases/Indices_completos/Stocks_Brazil.csv")
-can_csv <- read.csv("C:/Users/jpber/OneDrive/Documents/BanRep/Replicacion/Bases/Indices_completos/Stocks_Canada.csv")
-chl_csv <- read.csv("C:/Users/jpber/OneDrive/Documents/BanRep/Replicacion/Bases/Indices_completos/Stocks_Chile.csv")
-dnk_csv <- read.csv("C:/Users/jpber/OneDrive/Documents/BanRep/Replicacion/Bases/Indices_completos/Stocks_Denmark.csv")
-fin_csv <- read.csv("C:/Users/jpber/OneDrive/Documents/BanRep/Replicacion/Bases/Indices_completos/Stocks_Finland.csv")
-fra_csv <- read.csv("C:/Users/jpber/OneDrive/Documents/BanRep/Replicacion/Bases/Indices_completos/Stocks_France.csv")
-deu_csv <- read.csv("C:/Users/jpber/OneDrive/Documents/BanRep/Replicacion/Bases/Indices_completos/Stocks_Germany.csv")
-hkg_csv <- read.csv("C:/Users/jpber/OneDrive/Documents/BanRep/Replicacion/Bases/Indices_completos/Stocks_HongKong.csv") 
-ind_csv <- read.csv("C:/Users/jpber/OneDrive/Documents/BanRep/Replicacion/Bases/Indices_completos/Stocks_India.csv")
-idn_csv <- read.csv("C:/Users/jpber/OneDrive/Documents/BanRep/Replicacion/Bases/Indices_completos/Stocks_Indonesia.csv")
-mex_csv <- read.csv("C:/Users/jpber/OneDrive/Documents/BanRep/Replicacion/Bases/Indices_completos/Stocks_Mexico.csv")
-nor_csv <- read.csv("C:/Users/jpber/OneDrive/Documents/BanRep/Replicacion/Bases/Indices_completos/Stocks_Norway.csv")
-nld_csv <- read.csv("C:/Users/jpber/OneDrive/Documents/BanRep/Replicacion/Bases/Indices_completos/Stocks_Netherlands.csv")
+## List of countries
+countries <- c("Australia","Belgium", "Brazil", "Canada", "Chile", "Denmark", "Finland",
+               "France", "Germany", "HongKong", "India", "Indonesia","Mexico","Poland","Russia","SouthAfrica",
+               "SouthKorea", "Spain", "Sweden","Switzerland","Thailand","Turkey", "UnitedKingdom","USA1","USA2")
+
+## Loop through countries
+for (country in countries) {
+  ## Read CSV file
+  csv_file <- paste0("C:/Users/jpber/OneDrive/Documents/BanRep/Replicacion/Bases/Indices_completos/Stocks_", country, ".csv")
+  csv <- read.csv(csv_file)
   
-## Date class
-#aus_csv$Date <- as.Date(aus_csv$Date, "%m/%d/%Y")
-bel_csv$Date <- as.Date(bel_csv$Date, "%m/%d/%Y")
-bra_csv$Date <- as.Date(bra_csv$Date, "%m/%d/%Y")
-can_csv$Date <- as.Date(can_csv$Date, "%m/%d/%Y")
-chl_csv$Date <- as.Date(chl_csv$Date, "%m/%d/%Y")
-dnk_csv$Date <- as.Date(dnk_csv$Date, "%m/%d/%Y")
-fin_csv$Date <- as.Date(fin_csv$Date, "%m/%d/%Y")
-fra_csv$Date <- as.Date(fra_csv$Date, "%m/%d/%Y")
-deu_csv$Date <- as.Date(deu_csv$Date, "%m/%d/%Y")
-hkg_csv$Date <- as.Date(hkg_csv$Date, "%m/%d/%Y")
-ind_csv$Date <- as.Date(ind_csv$Date, "%m/%d/%Y")
-idn_csv$Date <- as.Date(idn_csv$Date, "%m/%d/%Y")
-mex_csv$Date <- as.Date(mex_csv$Date, "%m/%d/%Y")
-nor_csv$Date <- as.Date(nor_csv$Date, "%m/%d/%Y")
-nld_csv$Date <- as.Date(nld_csv$Date, "%m/%d/%Y")
+  ## Convert date class
+  csv$Date <- as.Date(csv$Date, "%m/%d/%Y")
+  
+  ## Convert to xts (time series)
+  xts_file <- paste0(tolower(country), "_xts")
+  assign(xts_file, xts(csv$Price, csv$Date))
+}
+
+
+base_test <- merge(australia_xts, belgium_xts,brazil_xts,canada_xts,chile_xts,denmark_xts,finland_xts,france_xts,
+                   germany_xts,hongkong_xts,india_xts,indonesia_xts,mexico_xts,poland_xts,russia_xts,southafrica_xts,
+                   southkorea_xts,spain_xts,sweden_xts,switzerland_xts,thailand_xts,turkey_xts,unitedkingdom_xts,
+                   usa1_xts,usa2_xts)
+
+weekends = c()
+for (i in 1:nrow(base_test)) {
+  row <- base_test[i, ]
+  if (sum(is.na(row))>=23){
+    weekends <- c(weekends, index(row))}
+}
+
+
+base_test2 <- base_test
+
+for (day in weekends){
+  base_test2 <- subset(base_test2, subset = index(base_test2) != day, drop = TRUE)
+}
+
+base <- na.locf(base_test$australia_xts)
+for(i in 2:25){base <- merge(base,na.locf(base_test[,i]))}
+#View(base)
+
+base2 <- na.locf(base_test2$australia_xts)
+for(i in 2:25){base2 <- merge(base2,na.locf(base_test2[,i]))}
+
+base_precios <- base2[49:nrow(base2),] #To remove NA values
+
+gdp <- read_excel("C:/Users/jpber/OneDrive/Documents/BanRep/Replicacion/Bases/GDP/GDP_AUSTRALIA.xlsx",sheet=1)
+
+for(country in countries){}
+gdp_australia <- td(gdp$Australia ~ 1, conversion = "sum", to = 365, method = "denton-cholette")
+
+
+base_retornos <- diff(log(base2)) #Non numeric ?
 
 
 
-## Convert to xts (time series)
-#aus_xts <- xts(aus_csv$Price, aus_csv$Date)
-bel_xts <- xts(bel_csv$Price, bel_csv$Date)
-bra_xts <- xts(bra_csv$Price, bra_csv$Date)
-can_xts <- xts(can_csv$Price, can_csv$Date)
-chl_xts <- xts(chl_csv$Price, chl_csv$Date)
-dnk_xts <- xts(dnk_csv$Price, dnk_csv$Date)
-fin_xts <- xts(fin_csv$Price, fin_csv$Date)
-fra_xts <- xts(fra_csv$Price, fra_csv$Date)
-deu_xts <- xts(deu_csv$Price, deu_csv$Date)
-hkg_xts <- xts(hkg_csv$Price, hkg_csv$Date)
-ind_xts <- xts(ind_csv$Price, ind_csv$Date)
-idn_xts <- xts(idn_csv$Price, idn_csv$Date)
-mex_xts <- xts(mex_csv$Price, mex_csv$Date)
-nor_xts <- xts(nor_csv$Price, nor_csv$Date)
-nld_xts <- xts(nld_csv$Price, nld_csv$Date)
-
-#base_test <- merge(aus_xts,belgium_xts,brazil_xts,can_xts)
-base_test <- merge(bel_xts,bra_xts,can_xts,chl_xts,dnk_xts,fin_xts,fra_xts,deu_xts,hkg_xts,ind_xts,idn_xts,mex_xts,
-                   nor_xts, nld_xts)
-
-#base <- na.locf(base_test$aus_xts)
-base <- na.locf(base_test$bel_xts)
-
-for(i in 2:11){base <- merge(base,na.locf(base_test[,i]))}
-View(base)
 
 
-##To check the date index respect to a series of all the dates removing weekends
-
-date_seq <- seq(min(bel_csv$Date), max(bel_csv$Date), by = 1)
-date_seq <- date_seq[!(weekdays(as.Date(date_seq)) %in% c("sábado", "domingo"))]
-
-dates_list <- as.list(index(base))  
-dates_sequence <- as.list(date_seq)
-
-x1 <- dates_list[!(dates_list %in% dates_sequence)] #Problem
-x2 <- dates_sequence[!(dates_sequence %in% dates_list)] #Check if all are international holidays            
