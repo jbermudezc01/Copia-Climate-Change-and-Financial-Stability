2450a58cc44141b7db9f0173e6116e75d3b3b3af
lfmelov <lfmelov@unal.edu.co>
2023-03-13 15:19:52 -0500

    Depuracion del prg

--- Correccion_Replicacion_climate_change.R
+++ Correccion_Replicacion_climate_change.R
@@ -118,59 +118,58 @@ sd       <- apply(Retornos, MARGIN=2, FUN=sd)
 Stats = cbind(min,max,mean,sd,skewness,kurtosis )
 print(Stats, digits=3)
 
-### Desagregacion temporal ====
+### ------------------------------------------   Desagregacion temporal ---------------------------------------------
 # Las variables a desagregar diariamente son el crecimiento del GDP trimestral y el crecimiento del FDI anual.
 
 ### Datos para GDP trimestral ====
 #Leer la base de datos, establecer el formato fecha y generar la base de datos en xts y la lista a ser desagregada
-gdp_countries      <- read.csv(paste0(Dir,"GDP_COUNTRIES.csv"), header = TRUE, sep = ";") 
-dates              <- as.Date(as.yearqtr(gdp_countries$Time)) #date format, se supone que existe una col llamada <Time> 
-#gdp_countries_xts <- xts(gdp_countries[,-1], order.by = dates) #Se quita la columna <Time>, se supone que esta de primeras
-#quarterly_series  <- series_list_function(gdp_countries_xts)
+gdp_countries      <- read.csv(paste0(Dir,"GDP_COUNTRIES.csv"), header = TRUE, sep = ";") #<<<--- Base de datos con GDPs
+dates.low.freq     <- as.Date(as.yearqtr(gdp_countries$Time),frac=1) #date format, se supone que existe una col llamada <Time>
 quarterly_series   <- as.list(gdp_countries[,-1]) #Se quita la columna de fechas y se genera una lista de sus columnas
 
-### Matriz de agregacion GDP trimestral =============================================================================
+### ----- Matriz de agregacion GDP trimestral <qtr_agr> ------
 
 # El metodo de chow-lin requiere una matriz de agregacion. Sin embargo, en la version fast se tiene en cuenta las 
 # diferencias de dias que puede haber en cada mes. Por tanto, es necesario crear una matriz de agregacion que 
 # tenga lo anterior en cuenta. 
 
 nrows <- length(quarterly_series[[1]])   ## Hay 76 trimestres en la base de datos, numero de datos trimestrales
-ncols <- nrow(base_precios)   ## No. de dias, con los retornos se pierde un dato, y
-                              ## al tomarle diferencias se perdera otro dato
+ncols <- nrow(base_precios)   ## No. de dias, con los retornos se pierde un dato, al tomarle diferencias se pierde otro dato
 
 qtr_agr <- matrix(0, nrow = nrows, ncol = ncols) #matriz de agregacion 
-dates   <- as.character(index(base_precios))
+dates.high.freq   <- as.character(index(base_precios)) #Fechas de freq alta
+cat('\nHigh frequency range:',range(dates.high.freq))
+cat('\nLow frequency range:'); range(dates.low.freq)
 
-## Extrae los meses en formato yyyy-mm sin repeticiones.
-meses <- unique(substr(dates,1,7))
+## Extrae los meses de alta freq. en formato yyyy-mm sin repeticiones.
+meses <- unique(substr(dates.high.freq,1,7))
 
 #Realizamos la matriz de agregacion usando la funcion days. En este caso los enteros i iran de 0 a 75, 
 # y de acuerdo con la función esto generara el primer a tercer mes de cada trimestre. (Explicar mejor)
 for(i in 0:(nrows-1))
-  qtr_agr <- days(i, qtr_agr,meses,dates)
+  qtr_agr <- days(x=i, m=qtr_agr, months=meses, dates=dates.high.freq)
 
+### Definicion de parametros usados en Chow Lin. 
+# <alpha>:            Coef. de persitencia del error del modelo  
+# <matriz_var_cov_0>: La matriz de var-cov del error del modelo
 
-### Ahora sigamos con la solucion de Chow Lin. 
-#Para poder usar la funcion, necesitamos el vector, alpha y la matriz de var covar
+vec_cte <- c(rep(1, ncols)) ## vector constante usado como variable indicadora, dado que no tenemos otra
+alpha_fast = 0.99999        #<<---- Parametro de la version fast de chow-Lin
 
-vec_cte <- c(rep(1, ncols)) ## vector constante, dado que no tenemos variable indicadora
-alpha_fast = 0.99999 #<<---- Parametro de la version fast de chow-Lin
-
-#generar la matriz de var-cov de acuerdo al paper analizado
-matriz_var_cov_0 <- matrix(0, nrow=ncols, ncol=ncols)
+#generar la matriz de var-cov de acuerdo al paper analizado (falta multiplicarla por sigma^2/(1-alpha^2) )
+matriz_var_cov_0 <- matrix(1, nrow=ncols, ncol=ncols)
 for(i in 1:ncols){
-  matriz_var_cov_0[i, i] <- 1
-  for(j in 1:ncols){
+  for(j in 1:ncols)
     if(j != i) matriz_var_cov_0[i, j] = alpha_fast^(abs(j-i))
-  }
 } ##Crear la matriz de var-cov
 
-## Usamos la funcion, lo que da una lista de las series desagregadas, y posteriormente las juntamos en una base
-gdp_growth_base <- chow_lin(quarterly_series, qtr_agr, vec_cte, alpha_fast, matriz_var_cov_0)
+## SE ejecuta la funcion de chow_lin, que da como resultado una base de datos de las series desagregadas.
+daily_series    = chow_lin(time_Series_list=quarterly_series, c=qtr_agr, w=vec_cte, var_covar=matriz_var_cov_0)
+gdp_growth_base = apply(daily_series, MARGIN=2, function(x) diff(log(x))) #diff(log(series))
+gdp_growth_base  = as.xts(gdp_growth_base, order.by=index(base_precios[-1,]))
 
-#Colocamos de nombres de las columnas los paises
-names <- colnames(base_retornos)
+#Colocamos los nombres de las series
+names                     <- colnames(base_retornos)
 colnames(gdp_growth_base) <- names[1:ncol(gdp_growth_base)]
 #Para diferenciar los nombres de los retornos le agregamos un prefijo gdp
 colnames(gdp_growth_base) <- paste("gdp",colnames(gdp_growth_base),sep="_")
@@ -181,38 +180,32 @@ colnames(gdp_growth_base) <- paste("gdp",colnames(gdp_growth_base),sep="_")
 #Ahora para hacer la desagregacion temporal del FDI necesitaremos los mismos cinco argumentos: una lista de las 
 # series a desagregar, un vector constante, una matriz de agregación y una matriz de varianzas covarianzas-
 # el alpha puede seguir siendo el mismo, ya que para el metodo fast debe ser 0.99999.
-
-fdi_countries <- read_xlsx(paste0(Dir,"FDI_anual.xlsx"), sheet="FDI")
-fdi_countries_ts <- as.ts(fdi_countries[,-1],start=2001,frequency=1)
-fdi_series <- series_list_function(fdi_countries_ts)
-
+fdi_countries    = read_xlsx(paste0(Dir,"FDI_anual.xlsx"), sheet="FDI") #<<--- Base datos de los FDI
+fdi_countries_ts = as.ts(fdi_countries[,-1],start=2001,frequency=1)     #<<--- Fecha y freq inicial de los datos 
+fdi_series       = as.list(fdi_countries_ts)
 
 ##Matriz de agregacion anual FDI 
-
-
 fdi_rows <- nrow(fdi_countries_ts)
 ##El numero de columnas es igual al de gdp porque se quiere desagregar en esa cantidad de dias (ncols)
-
 fdi_agregacion_matriz  <- matrix(0, nrow = fdi_rows, ncol = ncols)
 
-#con este ciclo generamos la matriz de agregacion, colocando uno a los dias que pertenezcan al año correspondiente.
-#Por ejemplo en la primera fila tendran uno aquellos dias que pertenezcan al 2001
-
+#Se genera la matriz de agregacion, colocando uno a los dias que pertenezcan al año correspondiente.
+#Por ejemplo en la primera fila tendran uno aquellos dias que pertenezcan al 2001 (primer anho)
+i1 = 0
 for(i in as.numeric(unique(substr(dates,1,4)))){
+  i1 = i1 + 1
   for(date in dates){
     if(substr(date,1,4)==i){
       pos <- which(dates == date)
-      fdi_agregacion_matriz[i-2000,pos] <- 1
+      fdi_agregacion_matriz[i1, pos] <- 1
     }
   }
 }
 
-
-# El vector constante depende del numero de dias a los cuales se quiere desagregar, que son los mismos por lo que se
-# puede utilizar la variable w. para alfa sirve alpha_fast. la matriz de varianzas y covarianzas tambien 
-# permaece igual
-
-fdi_growth_base <- chow_lin(fdi_series,fdi_agregacion_matriz,vec_cte,alpha_fast,matriz_var_cov_0)
+# <vec_cte> corresponde a la serie indicadora, al igual que la matriz de var-cov permanecen iguales a las usadas en los GDPs
+fdi_daily_series = chow_lin(fdi_series, fdi_agregacion_matriz, vec_cte, matriz_var_cov_0)
+fdi_growth_base  = apply(fdi_daily_series, MARGIN=2, function(x) diff(log(x))) #diff(log(series))
+fdi_growth_base  = as.xts(fdi_growth_base, order.by=index(base_precios[-1,]))
 
 #Colocamos los mismos nombres que la base de retornos pero le agregamos un prefijo gfdi
 colnames(fdi_growth_base) <- names[1:ncol(fdi_growth_base)]
@@ -220,14 +213,13 @@ colnames(fdi_growth_base) <- paste("gfdi",colnames(fdi_growth_base),sep="_")
 
 #Por otro lado, tambien es necesario reducir la muestra a las bases para que concuerden con la muestra de paper
 #Podemos usar la funcion que estaba anteriormente especificada
-
 Crecimiento_PIB <- gdp_growth_base[paste0(dia.inicial,"/"),]
 Crecimiento_FDI <- fdi_growth_base[paste0(dia.inicial,"/"),]
 
 ### Dummies corregidas =====
 
-# Corremos la función create_dummies en el archivo que contiene las fechas de las dummies
-dummies <- create_dummies(paste0(Dir,"EMDATA_dummies.xlsx")) 
+# Corremos la función create_dummies sobre el archivo que contiene las fechas de las dummies
+dummies <- create_dummies(excel_file=paste0(Dir,"EMDATA_dummies.xlsx")) 
 
 #Para cada tipo de desastre lo guardamos en un xts distinto
 biological_dummies           <- dummies$`Biological_dummies_xts}`
